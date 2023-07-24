45be6d982f5cc52529149372903602677ee9f9c4
SamatAlim <139941248+SamatAlim@users.noreply.github.com>
2023-07-24 20:45:01 +0600

    Add files via upload

---
+++ DZ4_SamatAlimbaev_27-01/app.js
@@ -0,0 +1,34 @@
+const wrapper = document.querySelector(".wrapper");
+
+const createCard = (user) => {
+    const userCard = document.createElement("div");
+    userCard.setAttribute("class", "userCard");
+    userCard.innerHTML = `
+        
+        <h2>${user.name}</h2>
+        <span>Age: ${user.age}</span>
+      `;
+    return userCard;
+};
+
+const renderUserCards = (data) => {
+    data.forEach((user) => {
+        const userCard = createCard(user);
+        wrapper.append(userCard);
+    });
+};
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "peoples.json");
+xhr.setRequestHeader("Content-type", "application/js");
+xhr.send();
+
+xhr.addEventListener('load', () => {
+    if (xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText);
+        renderUserCards(data);
+    } else {
+        console.error("Ошибка!", xhr.status);
+    }
+});
+
